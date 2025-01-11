6d33f0c183bc87d9dd22d2b7b50328b1d9e8e543
FredericBruening <contact@fredesk.com>
2025-01-11 17:58:16 -0500

    refactor: listen fn kept an open handle for tests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,17 +6,12 @@ import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("API is working!");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
 export default app;
