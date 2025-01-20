31285c0a682e8fe255029e9b8fa9193aed4d032b
Sunny Master <master.codeworks@gmail.com>
2025-01-20 13:35:06 -0500

    run prettier

--- frontend/src/components/Providers.tsx
+++ frontend/src/components/Providers.tsx
@@ -1,14 +1,12 @@
-'use client'
+"use client";
 
-import { StyleProvider } from '@ant-design/cssinjs'
-import { AntdRegistry } from "@ant-design/nextjs-registry"
+import { StyleProvider } from "@ant-design/cssinjs";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
-      <AntdRegistry>
-          <StyleProvider layer>
-              {children}
-          </StyleProvider>
-      </AntdRegistry>
-  )
+        <AntdRegistry>
+            <StyleProvider layer>{children}</StyleProvider>
+        </AntdRegistry>
+    );
 }
