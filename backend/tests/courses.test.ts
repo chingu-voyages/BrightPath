9c47ca3f048576f22ee9876a09278c63eaf875f2
FredericBruening <contact@fredesk.com>
2025-01-21 16:54:16 -0500

    add: endpoint for course by slug

--- backend/tests/courses.test.ts
+++ backend/tests/courses.test.ts
@@ -1,24 +1,31 @@
 import request from "supertest";
 import app from "../src/app";
 import { createContext } from "./context";
-import { createCourses } from "./utils";
+import { createPersistentCourse } from "./utils";
+import { Course } from "@prisma/client";
 
-const prisma = createContext().prisma;
+const ctx = createContext();
 
-describe("GET /courses", () => {
-    beforeEach(async () => {
-        await prisma.course.deleteMany();
-        await prisma.user.deleteMany();
-    });
-
+describe("GET /courses with no courses", () => {
     it("should return an empty array when no courses exist", async () => {
         const response = await request(app).get("/courses");
         expect(response.status).toBe(200);
         expect(response.body).toEqual([]);
     });
+});
+
+describe("GET /courses", () => {
+    let courses: Course[];
+
+    beforeEach(async () => {
+        await ctx.prisma.course.deleteMany();
+        await ctx.prisma.user.deleteMany();
+
+        courses = await createPersistentCourse(ctx, 2);
+    });
+
 
     it("should return a list of courses", async () => {
-        const courses = await createCourses(2);
 
         const response = await request(app).get("/courses");
         expect(response.status).toBe(200);
@@ -29,3 +36,27 @@ describe("GET /courses", () => {
         expect(course2.title).toBe(courses[1].title);
     });
 });
+
+describe("GET /courses/:slug", () => {
+    beforeEach(async () => {
+        await ctx.prisma.course.deleteMany();
+        await ctx.prisma.user.deleteMany();
+    });
+
+    it("should return a course by slug", async () => {
+        const [course] = await createPersistentCourse(ctx, 1);
+
+        const response = await request(app).get(`/courses/${course.slug}`);
+        expect(response.status).toBe(200);
+
+        const courseResponse = response.body;
+        expect(courseResponse.title).toBe(course.title);
+    });
+
+    it("should return 404 if course does not exist", async () => {
+        const response = await request(app).get("/courses/nonexistent-slug");
+        expect(response.status).toBe(404);
+    });
+});
+
+
