20fe7cd7e8f381ffdb54506d2f3d6b5b6990f0ae
varchodi <rauschenrobert@gmail.com>
2025-01-22 14:04:55 -0500

    default port

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
                     <Layout
                         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 md:px-20 flex flex-col justify-start items-start bg-slate-200 dark:bg-slate-900 lg:h-svh font-medium font-mono`}
                     >
-                        <Header className="flex justify-between text-lg md:text-2xl items-center w-full p-6 md:p-12  rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
+                        <Header className="flex justify-between text-lg md:text-2xl items-center w-full p-6 md:p-12  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
                             <div>Logo</div>
                             <nav className="flex justify-center items-center gap-4   ">
                                 <Link
@@ -62,7 +62,7 @@ export default function RootLayout({
                         {/* main contents */}
                         <main className="flex-1">{children}</main>
 
-                        <Footer className="flex  w-full p-6 md:px-12  rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
+                        <Footer className="flex  w-full p-6 md:px-12  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
                             {/* footer*/}
                         </Footer>
                     </Layout>
