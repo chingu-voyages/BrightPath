f96b7fca53a6ce7d2799b8cda69ce06908ffc9c8
varchodi <rauschenrobert@gmail.com>
2025-01-22 14:32:52 -0500

    rn logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
                         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 md:px-20 flex flex-col justify-start items-start bg-slate-200 dark:bg-slate-900 lg:h-svh font-medium font-mono`}
                     >
                         <Header className="flex justify-between text-lg md:text-2xl items-center w-full p-6 md:p-12  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
-                            <div>Logo</div>
+                            <div>BrightPath</div>
                             <nav className="flex justify-center items-center gap-4   ">
                                 <Link
                                     href={"/"}
