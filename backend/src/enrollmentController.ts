55ae1b5bdab49cee8bc1c0a8fe2e03ae2ff73b9a
FredericBruening <contact@fredesk.com>
2025-01-24 17:03:35 -0500

    add: enrollment router, controller, and tests

---
+++ backend/src/enrollmentController.ts
@@ -0,0 +1,37 @@
+import { Context } from "./context";
+import { EnrollmentStatus } from "@prisma/client";
+
+export async function getAllEnrollments(ctx: Context) {
+    return await ctx.prisma.enrollment.findMany({ include: { course: true, user: true } });
+}
+
+export async function getEnrollmentsByUserId(ctx: Context, userId: number) {
+    return await ctx.prisma.enrollment.findMany({
+        where: { userId },
+        include: { course: true, user: true },
+    });
+}
+
+export async function createEnrollment(ctx: Context, courseId: number, userId: number) {
+    return await ctx.prisma.enrollment.create({
+        data: {
+            courseId,
+            userId,
+        },
+        include: { course: true, user: true },
+    });
+}
+
+export async function updateEnrollment(ctx: Context, id: number, status: EnrollmentStatus) {
+    return await ctx.prisma.enrollment.update({
+        where: { id: id },
+        data: { status }
+    });
+}
+
+export async function deleteEnrollment(ctx: Context, id: number) {
+    return await ctx.prisma.enrollment.delete({
+        where: { id: id },
+        select: { id: true },
+    });
+}
