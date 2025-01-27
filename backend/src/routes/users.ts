721baeb2908bf19db46872af0dcc03ea1e060a71
ProgramStuff <jordanpkelsey1@gmail.com>
2025-01-27 15:57:51 -0400

    Update user signin endpoint
    Moved all db logic for user signin and updated request to include password and account

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,7 +6,8 @@ const ctx = createContext();
 
 // POST /signin
 router.post("/signin", async (req, res) => {
-    const { email, name, image } = req.body;
+    const { email, name, image, password, account } = req.body;
+
     try {
         let user = await ctx.prisma.user.findUnique({
             where: { email: email },
@@ -16,11 +17,27 @@ router.post("/signin", async (req, res) => {
             user = await ctx.prisma.user.create({
                 data: {
                     email,
+                    password,
                     name,
                     image,
                     role: "STUDENT",
                 },
             });
+
+            if (account) {
+                await ctx.prisma.account.create({
+                    data: {
+                        provider: account.provider,
+                        providerAccountId: account.providerAccountId,
+                        userId: user.id,
+                    },
+                });
+            }
+        }
+        if (user && password) {
+            if (password !== user.password) {
+                res.status(500).json({ error: "Incorrect credentials" });
+            }
         }
         res.status(200).json(user);
     } catch (error) {
