9081ab2609310e0f1180f42c657130e99f559576
ProgramStuff <jordanpkelsey1@gmail.com>
2025-01-27 13:23:51 -0400

    Update: profile page
    Update page to fetch session and render the users name

--- frontend/src/app/user/profile/page.tsx
+++ frontend/src/app/user/profile/page.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import Link from "next/link";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { useEffect, useState } from "react";
 
 export default function Profile() {
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        const fetchSession = async () => {
+            const response = await fetch("/auth/session");
+            if (response.ok) {
+                const session = await response.json();
+                setName(session.user.name);
+            } else {
+                setName("");
+            }
+        };
+
+        fetchSession();
+    }, []);
     return (
-        <Link
-            href={"/user/settings"}
-            className="text-slate-700 dark:text-slate-100 hover:opacity-75 delay-500"
-        >
-            <div>
-                <SettingsOutlinedIcon />
-            </div>
-        </Link>
+        <div>
+            {name ? (
+                <h2>Welcome, {name}!</h2> // Display the username
+            ) : (
+                <h2>Please sign in</h2>
+            )}
+
+            <Link
+                href={"/user/settings"}
+                className="text-slate-700 dark:text-slate-100 hover:opacity-75 delay-500"
+            >
+                <div>
+                    <SettingsOutlinedIcon />
+                </div>
+            </Link>
+        </div>
     );
 }
