394656ea9aa61272bfb3199d386512834dbbef57
FredericBruening <contact@fredesk.com>
2025-01-28 10:19:11 -0500

    switch to vitest for better compatibility

--- frontend/tests/setupTests.ts
+++ frontend/tests/setupTests.ts
@@ -1,9 +1,14 @@
+import "whatwg-fetch";
 import { setupServer } from "msw/node";
 import { handlers } from "./mocks/handlers";
-import "whatwg-fetch";
+import { afterAll, afterEach, beforeAll } from 'vitest';
+import { cleanup } from "@testing-library/react";
 
 const server = setupServer(...handlers);
 
 beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+    cleanup();
+    server.resetHandlers()
+});
 afterAll(() => server.close());
