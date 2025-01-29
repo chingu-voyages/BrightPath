ed0eb3a8177b7b0155d21900f9b37636f39da557
ProgramStuff <jordanpkelsey1@gmail.com>
2025-01-28 20:29:08 -0400

    Update middleware
    Add protected user routes and signin route

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,9 +1,22 @@
-export { auth as middleware } from "auth";
+import { auth } from "auth";
+import { NextRequest, NextResponse } from "next/server";
 
-// Or like this if you need to do something here.
-// export default auth((req) => {
-//   console.log(req.auth) //  { session: { user: { ... } } }
-// })
+export async function middleware(request: NextRequest) {
+    const session = await auth();
+
+    const reqPath = request.nextUrl.pathname;
+
+    const protectedRoutes = ["/user/profile", "user/setting"];
+    if (reqPath == "/auth/signin" && session?.user) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (protectedRoutes.includes(reqPath) && session?.user == undefined) {
+        return NextResponse.redirect(new URL("/auth/signin", request.url));
+    }
+
+    return NextResponse.next();
+}
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
