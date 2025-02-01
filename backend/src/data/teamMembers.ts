8e9fbada070e643414684912c74b87da7f8c1094
Sunny Master <master.codeworks@gmail.com>
2025-01-31 19:40:14 -0500

    add our-team page

---
+++ backend/src/data/teamMembers.ts
@@ -0,0 +1,24 @@
+const teamMembers = [
+    {
+        id: 1,
+        name: "Joseph Kotvas",
+        role: "Product Designer & Educator",
+        profilePicture: "https://i.imgur.com/FztwTlV.jpg",
+        bio: "Joseph plans, designs, and produces high-impact digital experiences to delight users and drive business growth. With a background teaching students at every level from  Kindergarten through college, Joseph loves to create tools to help people reach their full potential.",
+        linkedin: "https://www.linkedin.com/in/joekotvas/",
+        github: "https://github.com/joekotvas",
+        personalWebsite: "https://jokma.com/",
+    },
+    {
+        id: 2,
+        name: "Sunny Master",
+        role: "Full Stack Developer",
+        profilePicture: "https://i.imgur.com/LnOKOTJ.jpg",
+        bio: "Sunny builds intuitive, responsive web applications that enhance user experiences and streamline workflows. With a background in customer service and sales, he understands user needs and creates solutions that drive engagement. Sunny thrives on collaboration and continuous learning to craft impactful digital products.",
+        linkedin: "https://www.linkedin.com/in/sunnymaster",
+        github: "https://github.com/Sunny-Master",
+        personalWebsite: "https://sunnymaster.dev",
+    },
+];
+
+export default teamMembers;
