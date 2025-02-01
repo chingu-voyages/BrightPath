143cdc80f4bd9370b7108ff15ce7fed3399d2f59
varchodi <rauschenrobert@gmail.com>
2025-02-01 14:57:13 -0500

    Center stuffs

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -47,14 +47,14 @@ export default async function RootLayout({
                     <Layout
                         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 flex flex-col justify-start items-start bg-slate-200 dark:bg-slate-900 lg:h-svh font-medium font-mono`}
                     >
-                        <Header className="flex justify-between text-lg md:text-2xl items-center w-full p-6 md:p-12  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
+                        <Header className="flex justify-between text-lg md:text-2xl items-center w-full p-6 md:p-12 lg:px-28 xl:px-48 2xl:px-72  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
                             <Link href={"/"} className="font-semibold">
                                 BrightPath
                             </Link>
                             <nav className="flex justify-center items-center gap-4   ">
                                 <Link
                                     href={"/"}
-                                    className="text-slate-700 dark:text-slate-100 hover:opacity-75 hover:underline delay-500"
+                                    className="text-slate-700 dark:text-slate-100 hover:opacity-75 hover:underline delay-500 hidden md:block"
                                 >
                                     Home
                                 </Link>
@@ -80,9 +80,9 @@ export default async function RootLayout({
                             </nav>
                         </Header>
                         {/* main contents */}
-                        <main className="flex-1 w-screen">{children}</main>
+                        <main className="flex-1 lg:px-28 xl:px-48 2xl:px-72">{children}</main>
 
-                        <Footer className="flex  w-full p-6 md:px-12  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
+                        <Footer className="flex  w-full p-6 md:px-12 lg:px-28 xl:px-48 2xl:px-72  rounded-md md:rounded-xl shadow-sm border border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-800">
                             {/* footer*/}
                         </Footer>
                     </Layout>
