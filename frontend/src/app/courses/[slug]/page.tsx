16e1fa133d354a7c9fc33ddaf45ab506042dae68
FredericBruening <contact@fredesk.com>
2025-02-03 18:14:21 -0500

    add dayjs, draft durations

--- frontend/src/app/courses/[slug]/page.tsx
+++ frontend/src/app/courses/[slug]/page.tsx
@@ -1,13 +1,59 @@
-import { Enrollment, Prisma } from "@prisma/client";
+import { Assignment, Enrollment, Prisma } from "@prisma/client";
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { auth } from "@/auth";
 import Link from "next/link";
 import EnrollButton from "./EnrollButton";
 
-type Course = Prisma.CourseGetPayload<{
-    include: { instructor: true; units: true };
-}>;
+dayjs.extend(duration);
+dayjs.extend(relativeTime)
 
-type Unit = Prisma.UnitGetPayload<{}>;
+type Unit = Prisma.UnitGetPayload<{
+    include: { assignments: true };
+}> & { duration: string };
+
+type Course = Prisma.CourseGetPayload<{
+    include: { instructor: true; units: { include: { assignments: true } } };
+}> & { duration: string, units: Unit[] };
+
+
+const computeCourseDuration = (course: any) => {
+    const totalDuration = dayjs.duration(0);
+
+    course.units.forEach((unit: any) => {
+        const unitDuration = computeDurationForUnit(unit);
+        totalDuration.add(unitDuration);
+    });
+
+    course.duration = totalDuration.humanize();
+};
+
+const computeDurationForUnit = (unit: Unit) => {
+    let totalDuration = dayjs.duration(0);
+    unit.assignments.forEach((assignment: Assignment) => {
+        const parsedDuration = dayjs(assignment.duration);
+        const duration = dayjs.duration({
+            hours: parsedDuration.hour(),
+            minutes: parsedDuration.minute(),
+        });
+
+
+        totalDuration = totalDuration.add(duration);
+    });
+
+    unit.duration = totalDuration.humanize();
+
+    return totalDuration
+};
+
+const formatDuration = (duration: string | Date): string => {
+    const d = dayjs(duration, 'HH:mm')
+    const hours = d.hour();
+    const minutes = Math.round(d.minute() / 5) * 5;
+
+    return dayjs().hour(hours).minute(minutes).format('HH:mm');
+};
 
 export const dynamic = "force-dynamic";
 
@@ -19,6 +65,7 @@ export default async function Courses({
     const slug = (await params).slug;
     const res = await fetch(process.env.BACKEND_API_URL + "/courses/" + slug);
     const course: Course = await res.json();
+    computeCourseDuration(course);
 
     const session = await auth();
     const user = session?.user;
@@ -131,9 +178,31 @@ export default async function Courses({
                 <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-2">
                     Course Outline
                 </h3>
-                <ul className="list-disc list-inside text-sm text-gray-500 dark:text-gray-400 space-y-1">
-                    {course.units?.map((unit: Unit) => (
-                        <li key={unit.id}>{unit.title}</li>
+                <ul className="list-disc list-inside text-sm text-black space-y-1">
+                    {course.units?.map((unit: Unit, index) => (
+                        <div key={unit.id} className="mb-4">
+                            <div className="flex items-center mb-2">
+                                <p>Unit {index + 1}</p>
+                                <p>{unit.duration}</p>
+                            </div>
+
+                            <h3>{unit.title}</h3>
+                            <p>{unit.description}</p>
+                            {unit.assignments?.map((assignment: Assignment) => (
+                                <div key={assignment.id} className="mb-4 bg-gray-200 rounded-lg p-4">
+                                    <div className="flex items-center mb-2">
+                                        <p>{assignment.type}</p>
+                                        <p>{formatDuration(assignment.duration)}</p>
+                                    </div>
+
+                                    <div className="flex items-center justify-between">
+                                        <h4>{assignment.title}</h4>
+                                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
                     ))}
                 </ul>
             </div>
