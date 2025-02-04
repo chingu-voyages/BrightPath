c2e7d501fc6cbf0b7c109287b28517d56257c38a
FredericBruening <contact@fredesk.com>
2025-02-04 10:49:20 -0500

    fix type errors

--- frontend/src/app/user/settings/page.tsx
+++ frontend/src/app/user/settings/page.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { Form, Input, Button, Upload, message } from "antd";
 import { Upload as UploadIcon } from "@mui/icons-material";
 import { UploadChangeParam } from "antd/es/upload";
-import { Prisma } from "@prisma/client";
 import { auth } from "@/auth";
 
-type UserData = Prisma.UserGetPayload<{}>;
+import { type User } from "next-auth";
+
+type UserData = User;
 
 export const dynamic = "force-dynamic";
 
