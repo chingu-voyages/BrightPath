46abd922b1f7827b0943e0405a427a3b869cdda5
ProgramStuff <jordanpkelsey1@gmail.com>
2025-02-05 19:05:37 -0400

    Update course card
    Modify course card to resemble wireframe.

--- frontend/src/app/courses/page.tsx
+++ frontend/src/app/courses/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import { type Prisma } from "@prisma/client";
 import { computeCourseDuration } from "@/lib/utils";
+import SignalCellularAlt1BarIcon from "@mui/icons-material/SignalCellularAlt1Bar";
+import SignalCellularAlt2BarIcon from "@mui/icons-material/SignalCellularAlt2Bar";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { Tag, Avatar } from "antd";
+
+import moment from "moment";
 
 type Course = Prisma.CourseGetPayload<{
     include: { instructor: true };
@@ -11,7 +18,6 @@ export const dynamic = "force-dynamic";
 export default async function Courses() {
     const res = await fetch(process.env.BACKEND_API_URL + "/courses");
     const courses: Course[] = await res.json();
-
     for (const course of courses) {
         computeCourseDuration(course);
     }
@@ -21,16 +27,27 @@ export default async function Courses() {
             {courses.map((course: Course) => (
                 <Link
                     key={course.id}
-                    className="p-4 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 dark:bg-gray-800 bg-white"
+                    className="grid p-4 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200 dark:bg-gray-800 bg-white"
                     data-testid="course"
                     href={`/courses/${course.slug}`}
                 >
                     <div className="flex justify-between items-center mb-2">
                         <div className="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
+                            {course.difficulty == "BEGINNER" && (
+                                <SignalCellularAlt1BarIcon />
+                            )}
+                            {course.difficulty == "INTERMEDIATE" && (
+                                <SignalCellularAlt2BarIcon />
+                            )}
+                            {course.difficulty == "ADVANCED" && (
+                                <SignalCellularAltIcon />
+                            )}
+
                             {course.difficulty}
                         </div>
                         <span className="text-sm text-gray-500 dark:text-gray-400">
-                            {course.duration}
+                            <AccessTimeIcon className="mr-2" />
+                            {moment.duration(course.duration).humanize()}
                         </span>
                     </div>
                     <div className="relative h-40 mb-2 overflow-hidden rounded-lg">
@@ -45,13 +62,38 @@ export default async function Courses() {
                     <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-1">
                         {course.title}
                     </h2>
+                    <div className="flex-wrap items-center">
+                        {/* Hard coded values to be removed later */}
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Code
+                        </Tag>
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Design
+                        </Tag>
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Tailwind
+                        </Tag>
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Tailwind
+                        </Tag>
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Tailwind
+                        </Tag>
+                        <Tag className="mb-2 dark:bg-gray-800 text-gray-500 dark:text-gray-400">
+                            Tailwind
+                        </Tag>
+                    </div>
                     <p className="text-sm text-gray-500 dark:text-gray-400 mb-2">
                         {course.shortDescription}
                     </p>
 
-                    <div className="flex justify-between items-center mt-4">
+                    <div className="flex justify-end self-end">
                         <span className="text-sm text-gray-500 dark:text-gray-400">
                             {course?.instructor?.name}
+                            <Avatar
+                                className="ml-2"
+                                src={course.instructor.image}
+                            />
                         </span>
                     </div>
                 </Link>
