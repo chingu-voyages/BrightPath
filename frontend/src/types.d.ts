0aa7780ab84394f77aaadc0f20a628956686b6b0
FredericBruening <contact@fredesk.com>
2025-02-06 19:04:26 -0500

    add mising types, update: course type

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,5 +1,11 @@
 import { Prisma } from "@prisma/client";
 
 export type Course = Prisma.CourseGetPayload<{
-    include: { instructor: true };
+    include: { instructor: true, units: { include: { assignments: true } } };
+}> & { duration: number, units: Unit[] };
+
+type Unit = Prisma.UnitGetPayload<{
+    include: { assignments: true };
 }> & { duration: number };
+
+
