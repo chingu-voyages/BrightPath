614eb6c5ad8530d0ec22a7f382b7d44a92603f4c
varchodi <rauschenrobert@gmail.com>
2025-02-07 18:38:19 -0500

    finish Horizontal component

--- frontend/src/app/user/dashboard/page.tsx
+++ frontend/src/app/user/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { auth } from '@/auth';
+import HorizontalCourseCard from '@/components/Course/HorizontalCourseCard';
+import { Course, Enrollment, User } from '@prisma/client';
 import { Metadata } from 'next';
 import React from 'react'
 
@@ -6,17 +9,33 @@ export const metadata: Metadata = {
     description: "Overwiew of courses...",
 };
 
-const Dashboard = async() => {
+type EnrollmelntOutput = Enrollment & { course: Course, user: User };
+
+
+const Dashboard = async () => {
+  const session = await auth()!;
+  const { user } = session!;
+  // enrolls 
+  const enrollments: EnrollmelntOutput[] = await (await fetch(`${process.env.BACKEND_API_URL}/user/${user?.id}/enrollments`)).json();
+  const finished = enrollments.filter( enrollement=> enrollement.status === "COMPLETED");
+  const currents = enrollments.filter(enrollement => enrollement.status === "ACTIVE");
+
   return (
-    <div className='py-6'>
+    <div className='py-6 flex flex-col items-center gap-2'>
         {/* intro */}
-          <p className='text-lg italic'>
-              “Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come.” - Dwayne Johnson
-          </p>
           {/* in progress */}
-          <section>
-              <h1>Continue Learning</h1>
-              
+          <section className='bg-slate-50 rounded-lg max-w-fit p-6 border-b border-slate-300'>
+          <p className='text-lg italic md:w-8/12 border-b border-slate-300 mb-4 w-full'>
+              “Success isn't always about greatness. It's about consistency. <br />Consistent hard work leads to success. Greatness will come.” - Dwayne Johnson
+          </p>
+              <h1 className='text-3xl font-bold mb-4'>Continue Learning</h1>
+              <div className='flex flex-col gap-4 '>
+                {
+                currents.map((enrollement) => (
+                        <HorizontalCourseCard key={enrollement.id} {...enrollement} />
+                      ))
+                }
+              </div>
           </section>
 
           <section>
--- frontend/src/components/Course/HorizontalCourseCard.tsx
+++ frontend/src/components/Course/HorizontalCourseCard.tsx
@@ -1,68 +1,89 @@
-import { Course } from '@prisma/client';
+import { Course, Enrollment, User } from '@prisma/client';
 import { CheckCircleOutlined, DesktopOutlined, RightOutlined } from "@ant-design/icons";
 import Link from 'next/link';
 import React from 'react'
-
-const HorizontalCourseCard = async ({course,completed=false}:{course:Course,completed:boolean}) => {
+import { Image, Progress } from 'antd';
+// import Image from 'next/image';
+type  Proptype = Enrollment & { course: Course, user: User }
+const HorizontalCourseCard = async (props:Proptype) => {
     // const res = await fetch(`${process.env.BACKEND_API_URL}/courses/${id}`);
     // const course: Course = await res.json();
+
+    const { course, user,status,progress } = props;
+    const completed = status === "COMPLETED" && true;
+    
   return (
-    <article>
-      <div>
-        
-      </div>
-      <section>
-        <div>
-            <h1>{course.title}</h1>
-            {/* instructor ?? name */}
-            <p>taught by Instructor Name</p>
-        </div>
-        <div>
-            <p>{course.shortDescription}</p>
-            
-            {/* btn ?? */}
-            {
-                completed ? (
-                    <Link href ={`/course/${course.id}`}>view Certificate</Link>
-                ):(
-                    <Link href={`/course/${course.id}`}>
-                        <div>
-                            <p>continue learning</p>
+    <article className='flex flex-col md:flex-row w-full max-w-5xl h-full lg:h-56 border-2 border-slate-400  rounded-md '>
+        <figure className="relative h-72 lg:h-full w-full md:w-64 mb-2 overflow-hidden">
+            <img
+                src={course.thumbnail}
+                alt={course.title}
+                width={200}
+                height={200}
+                className="absolute inset-0 object-cover object-center w-full h-full"
+            />
+         </figure>
+         {/*?? details Instructors */}
+        <div className='p-4'>
+            <div>
+                  <h1 className='text-2xl font-semibold'>{course.title}</h1>
+                  <p>Taught by Instructor ????</p>
+              </div>
+              
+              <div className='w-full flex flex-col lg:flex-row justify-between gap-4 lg:items-center'>
+                  <p className='lg:w-3/4'>{course.shortDescription}</p>
+                  {/* ??continue btn or ... */}
+                  {
+                      completed ? (
+                          <Link className='max-w-fit lg:w-full p-6 text-center rounded-md font-semibold text-lg text-slate-100 dark:text-slate-800 bg-slate-700 dark:bg-slate-400' href={'/cerificate/??'}>
+                              View certificate
+                          </Link>
+                      ):<Link className='flex gap-2 p-2 px-4 rounded-md min-w-fit w-1/3 lg:w-1/3 text-slate-100 dark:text-slate-800  bg-slate-700 dark:bg-slate-400' href={`/course/${course.id}`}>
                             <div>
-                                <p>
-                                    <DesktopOutlined />
-                                </p>
-                                {/* Course last ... */}
-                                <p>
-                                    The Agile process
-                                </p>
+                                <p className='text-sm italic font-thin'>Continue learning:</p>
+                                
+                                <div className='text-lg font-semibold flex justify-center items-center gap-2'>
+                                    {/* <p>
+                                        
+                                    </p> */}
+                                    <DesktopOutlined style={{fontSize:24,padding:0}} />
+                                    {/* ??last chap */}
+                                    <p className=''>The Agile Process</p>
+                                </div>
                             </div>
-                        </div>
-                        <div>
-                        <RightOutlined />
-                        </div>
-                    </Link>
-                )
-            }
-        </div>
-            {
-                completed ? (
-                    // ?? completed date
-                    <div>
-                    <CheckCircleOutlined />   
-                    Completed on  Data xx xxx xxx
-                    </div>
+                            
+                            <RightOutlined style={{fontSize:12}} />
+                      </Link>
+                  }
+              </div>
+
+              {
+                  completed ? (
+                      <div>
+                          {/* ?? completed time */}
+                          <CheckCircleOutlined /> 
+                          Completed on date mm dd, yyyy
+                      </div>
                   ) : (
-                    <div>
-                        <p>progress</p>
-                        <div>
-                            <p>xx %</p>
-                            <p>xx hours left</p>
-                        </div>
+                    <div className='flex flex-col sm:flex-row justify-between lg:items-center'>
+                        <Progress
+                        className='w-full sm:w-3/4'
+                        percent={progress}
+                        size={'default'}
+                        strokeColor={"gray"}
+                        percentPosition={{
+                          align: 'end',
+                          type: 'outer',
+                        }}
+                        
+                      />
+                      
+                    {/* ?? remaining time */}
+                    <p className='font-semibold'>xx time left</p>
                     </div>
-                )
-            }
-      </section>
+                  )
+              }
+        </div>
     </article>
   )
 }
