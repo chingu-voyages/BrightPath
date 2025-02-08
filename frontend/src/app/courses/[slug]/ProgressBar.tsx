c616f2ea25cb2109ab845fff872ce608790db484
FredericBruening <contact@fredesk.com>
2025-02-07 19:53:01 -0500

    update: antd progress bar

--- frontend/src/app/courses/[slug]/ProgressBar.tsx
+++ frontend/src/app/courses/[slug]/ProgressBar.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useContext } from 'react';
 import { CoursePageContext } from "./Course";
+import { Progress } from "antd";
 
 export default function ProgressBar() {
     const { enrolled } = useContext(CoursePageContext);
@@ -15,15 +16,8 @@ export default function ProgressBar() {
 
     return (
         <div className="flex items-center">
-            <div className="w-full bg-gray-200 rounded">
-                <div
-                    className="h-2 bg-blue-500 rounded"
-                    style={{ width: `${progress}%` }}
-                />
-            </div>
-            <p className="pl-3 text-sm">
-                {progress}%
-            </p>
+            <Progress percent={progress} />
+
             <p className="pl-4 text-sm tracking-tight text-nowrap">
                 1 hour left
             </p>
