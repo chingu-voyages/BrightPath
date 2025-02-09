d8dfcacf6ccb1f268f50717a4e8a62634f7a942c
varchodi <rauschenrobert@gmail.com>
2025-02-09 08:26:43 -0500

    fix prettier

--- frontend/src/app/user/enrollment/[enrollmentId]/[unitId]/[assigmentId]/page.tsx
+++ frontend/src/app/user/enrollment/[enrollmentId]/[unitId]/[assigmentId]/page.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const AssigmentPage = async ({ params }: { params: Promise<{ enrollmentId: string,unitId:string, assigmentId: string }> }) => {
-  const { unitId, enrollmentId, assigmentId } = await params;
-  
-  return (
-    <div>
-      <h1>{unitId}</h1>
-          <h1>{enrollmentId}</h1>
-          <h1>{assigmentId}</h1>
-    </div>
-  )
-}
+const AssigmentPage = async ({
+    params,
+}: {
+    params: Promise<{
+        enrollmentId: string;
+        unitId: string;
+        assigmentId: string;
+    }>;
+}) => {
+    const { unitId, enrollmentId, assigmentId } = await params;
 
-export default AssigmentPage
+    return (
+        <div>
+            <h1>{unitId}</h1>
+            <h1>{enrollmentId}</h1>
+            <h1>{assigmentId}</h1>
+        </div>
+    );
+};
+
+export default AssigmentPage;
