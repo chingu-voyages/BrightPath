504b193a5f9308bd76e786f00ca8c3037434c67b
FredericBruening <contact@fredesk.com>
2025-02-08 19:10:54 -0500

    add assignment

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -6,8 +6,17 @@ export type Course = Prisma.CourseGetPayload<{
 
 export type Unit = Prisma.UnitGetPayload<{
     include: { assignments: true };
-}> & { duration: number };
+}> & { duration: number, assignments: Assignment[] };
 
 export type Enrollment = Prisma.EnrollmentGetPayload<{
     include: { course: true; user: true };
 }> & { course: Course };
+
+export type Assignment = Prisma.AssignmentGetPayload<{
+    include: { 
+        ReadingAssignment: true,
+        VideoAssignment: true,
+        InteractiveAssignment: true,
+        QuizAssignment: true,
+    };
+}>;
