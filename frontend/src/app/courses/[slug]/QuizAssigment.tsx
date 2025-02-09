7086dd17bdf1ea64574dac97904661acddcdcecd
varchodi <rauschenrobert@gmail.com>
2025-02-09 10:58:09 -0500

    runn prettier

--- frontend/src/app/courses/[slug]/QuizAssigment.tsx
+++ frontend/src/app/courses/[slug]/QuizAssigment.tsx
@@ -1,20 +1,18 @@
-import { Assignment } from '@prisma/client'
-import React from 'react'
+import { Assignment } from "@prisma/client";
+import React from "react";
 
 const QuizAssigment = ({ assigment }: { assigment: Assignment }) => {
-    // get questions stuffs 
-  return (
-    <article>
-      <section>
-        <h1>Quizzy</h1>
-      </section>
-      <section>
-        
-      </section>
-      <section></section>
-      <section></section>
-    </article>
-  )
-}
+    // get questions stuffs
+    return (
+        <article>
+            <section>
+                <h1>Quizzy</h1>
+            </section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </article>
+    );
+};
 
-export default QuizAssigment
+export default QuizAssigment;
