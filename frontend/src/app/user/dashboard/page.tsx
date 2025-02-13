63212b7ed790196612e2046d46df1fec8eb90a05
FredericBruening <contact@fredesk.com>
2025-02-12 21:31:35 -0500

    update: dashboard

--- frontend/src/app/user/dashboard/page.tsx
+++ frontend/src/app/user/dashboard/page.tsx
@@ -1,95 +1,83 @@
-import CourseCard from "@/app/courses/CourseCard";
 import { auth } from "@/auth";
+import CourseCard from "@/app/courses/CourseCard";
 import HorizontalCourseCard from "@/components/Course/HorizontalCourseCard";
-import { Enrollment } from "@/types";
 import { MoreHorizOutlined } from "@mui/icons-material";
-import { Course, EnrollmentStatus, Prisma, User } from "@prisma/client";
+import { EnrollmentStatus } from "@prisma/client";
+import { Course, Enrollment } from "@/types";
 import { Metadata } from "next";
-import React from "react";
+import { computeCourseDuration, getRandomQuote } from "@/lib/utils";
 
 export const metadata: Metadata = {
     title: "Student dashboard",
-    description: "Overwiew of courses...",
+    description: "Course overview",
 };
 
 const Dashboard = async () => {
-    const session = await auth()!;
-    const { user } = session!;
-    //!! still type buggy_ish
-    const recommandations: any[] = await (
-        await fetch(process.env.BACKEND_API_URL + "/courses/popular")
-    ).json();
+    const session = await auth();
+    const user = session?.user;
 
-    // enrolls
-    const enrollments: Enrollment[] = await (
-        await fetch(
-            `${process.env.BACKEND_API_URL}/user/${user?.id}/enrollments`,
-        )
-    ).json();
-    const finished = enrollments.filter(
-        (enrollement) => enrollement.status === EnrollmentStatus.COMPLETED,
-    );
-    const currents = enrollments.filter(
-        (enrollement) => enrollement.status === EnrollmentStatus.ACTIVE,
-    );
+    const [recommendations, enrollments]: [Course[], Enrollment[]] = await Promise.all([
+        fetch(`${process.env.BACKEND_API_URL}/courses/popular`).then(res => res.json()),
+        fetch(`${process.env.BACKEND_API_URL}/user/${user?.id}/enrollments`).then(res => res.json()),
+    ]);
+
+    for (const enrollment of enrollments) {
+        computeCourseDuration(enrollment.course);
+    }
+
+    const finished = enrollments.filter(enrollment => enrollment.status === EnrollmentStatus.COMPLETED);
+    const current = enrollments.filter(enrollment => enrollment.status === EnrollmentStatus.ACTIVE);
+
 
     return (
-        <div className="py-6 flex flex-col items-center gap-2">
-            <div className="max-w-fit ">
-                {/* intro */}
-                {/* in progress */}
-                <section className="bg-slate-50 rounded-lg max-w-fit p-6 border-b border-slate-300 mb-2">
-                    <p className="text-lg italic md:w-8/12 border-b border-slate-300 mb-4 w-full">
-                        “Success isn't always about greatness. It's about
-                        consistency. <br />
-                        Consistent hard work leads to success. Greatness will
-                        come.” - Dwayne Johnson
-                    </p>
-                    <h1 className="text-3xl font-bold mb-4">
-                        Continue Learning
-                    </h1>
-                    <div className="flex flex-col gap-4 ">
-                        {currents.map((enrollement) => (
+        <div className="my-6 flex flex-col gap-6">
+            <div>
+                <p className="text-xl text-gray-500 mb-8">
+                    {getRandomQuote()}
+                </p>
+                <h3 className="text-3xl font-bold mb-4">
+                    Continue Learning
+                </h3>
+                <div className="flex flex-col gap-4 ">
+                    {current.map((enrollement) => (
+                        <HorizontalCourseCard
+                            key={enrollement.id}
+                            enrollment={enrollement}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div>
+                <h3 className="text-3xl font-bold mb-4">
+                    Completed courses
+                </h3>
+                {finished.length > 0 ? (
+                    <div className="flex flex-col gap-4">
+                        {finished.map((enrollement) => (
                             <HorizontalCourseCard
                                 key={enrollement.id}
-                                {...enrollement}
+                                enrollment={enrollement}
                             />
                         ))}
                     </div>
-                </section>
-                <section className="bg-slate-50 rounded-lg max-w-full p-6 border-b border-slate-300 mb-2">
-                    <h1 className="text-3xl font-bold mb-4">
-                        Completed Course
-                    </h1>
-                    {finished.length > 0 ? (
-                        <div className="flex flex-col gap-4 ">
-                            {finished.map((enrollement) => (
-                                <HorizontalCourseCard
-                                    key={enrollement.id}
-                                    {...enrollement}
-                                />
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="w-full flex flex-col justify-center  items-center">
-                            <MoreHorizOutlined style={{ fontSize: 32 }} />
-                            <p className="italic">
-                                Nope; Still empty. Want a joke instead? (Just
-                                kidding...unless? 😁)
-                            </p>
-                        </div>
-                    )}
-                </section>
-
-                <section className="bg-slate-50 rounded-lg max-w-full p-6 border-b border-slate-300 mb-2">
-                    <h1 className="text-3xl font-bold mb-4">Recommandations</h1>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full max-w-5xl gap-4 ">
-                        {/* buggy_ish */}
-                        {recommandations.map((course: any) => (
-                            <CourseCard key={course.id} course={course} />
-                        ))}
+                ) : (
+                    <div className="w-full flex flex-col justify-center  items-center">
+                        <MoreHorizOutlined style={{ fontSize: 32 }} />
+                        <p className="italic">
+                            Nope; Still empty. Want a joke instead? (Just
+                            kidding...unless? 😁)
+                        </p>
                     </div>
-                </section>
+                )}
+            </div>
+
+            <div>
+                <h3 className="text-3xl font-bold mb-4">Recommendations</h3>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full max-w-5xl gap-4 ">
+                    {recommendations.map((course: any) => (
+                        <CourseCard key={course.id} course={course} />
+                    ))}
+                </div>
             </div>
         </div>
     );
